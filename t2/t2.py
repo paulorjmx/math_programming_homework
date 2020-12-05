845d48e4dc039f86665c2f71c9111f56f0a572a5
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-12-05 02:46:42 -0300

    Finished greedy heuristic

--- t2/t2.py
+++ t2/t2.py
@@ -3,92 +3,140 @@ from ortools.linear_solver import pywraplp
 from graphviz import Digraph
 import math as m
 
-def calcula_distancias(cidade, cidades):
-  distancias = []
-  for i in range(0, len(cidades)):
-    a = float(cidades[i][1]) - float(cidade[0])
-    b = float(cidades[i][2]) - float(cidade[1])
-    dist = m.sqrt((m.pow(a, 2) + m.pow(b, 2)))
-    #print(f'A distância entre os pontos {cidade[0]}, {cidade[1]} e {cidades[i][1]}, {cidades[i][2]} é: {dist}')
-    if dist == 0:
-      dist = 10000000000
-    distancias.append(dist)
+def search_min_cost(index, arr, visited, dim):
+	min = 10000000.0
+	node = -1
+	for i in range(0, dim):
+		if(arr[index][i] < min and visited[i] != True):
+			min = arr[index][i]
+			node = i
+	return node
 
-  return distancias
+def calcula_distancias(cidade, cidades):
+	distancias = []
+	for i in range(0, len(cidades)):
+		a = float(cidades[i][1]) - float(cidade[0])
+		b = float(cidades[i][2]) - float(cidade[1])
+		dist = m.sqrt((m.pow(a, 2) + m.pow(b, 2)))
+		#print(f'A distância entre os pontos {cidade[0]}, {cidade[1]} e {cidades[i][1]}, {cidades[i][2]} é: {dist}')
+		if dist == 0:
+			dist = 1000000.0
+		distancias.append(dist)
+	return distancias
 
 solver = pywraplp.Solver.CreateSolver('SCIP')
+parame = pywraplp.MPSolverParameters
+#solver = pywraplp.Solver('simple_lp_program',
+#                          pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+solver.set_time_limit(600000)
 
-ref_arq = open('wi29.tsp')
+ref_arq = open('../data_points/uruguay.tsp')
 
 cidades = []
 
 for line in ref_arq:
-  cidade = line.split()
-  cidades.append(cidade)
+	cidade = line.split()
+	cidades.append(cidade)
 
 custos = []
 
 for i in range(0, len(cidades)):
-  coord = (cidades[i][1], cidades[i][2])
-  distancia = calcula_distancias(coord, cidades)
-  custos.append(distancia)
+	coord = (cidades[i][1], cidades[i][2])
+	distancia = calcula_distancias(coord, cidades)
+	custos.append(distancia)
 
-'''
-CUSTO_INFINITO = 3000
+visitado = [False for i in range(0, len(cidades))]
+seq_vis = [-1 for i in range(0, len(cidades))]
+index = 0
+visitado[index] = True
+edge = 0
+for i in range(0, len(cidades)):
+	edge = search_min_cost(index, custos, visitado, len(cidades))
+	visitado[edge] = True
+	seq_vis[index] = edge
+	index = edge
 
-custos = [
-            [CUSTO_INFINITO, 10, 5, 85, 23],
-            [10, CUSTO_INFINITO, 45, 71, 99],
-            [5, 45, CUSTO_INFINITO, 5, 21],
-            [85, 71, 5, CUSTO_INFINITO, 9],
-            [23, 99, 21, 9, CUSTO_INFINITO],
-        ]
-'''
+# print(seq_vis)
+# for i in range(0, len(cidades)):
+#   for j in range(0, len(cidades)):
+#     print(str(i)+ ' -> ' +str(j)+ ' = ' +str(custos[i][j]))
+#   print("\n")
 
-num_planetas = len(custos)
+num_cidades = len(custos)
 
-dot = Digraph(comment='TSP Galaxies') # Cria o grafo direcionado
-dot.format = 'jpg' # Muda o formato do arquivo de saida
+#dot = Digraph(comment='TSP Galaxies') # Cria o grafo direcionado
+#dot.format = 'jpg' # Muda o formato do arquivo de saida
 
-for i in range(num_planetas): # Adiciona os nohs ao grafo
-    dot.node(str(i), str(i))
+#for i in range(num_planetas): # Adiciona os nohs ao grafo
+#    dot.node(str(i), str(i))
 
 
 # --------------- Definicoes das variaveis ------------------
 # x[i, j] corresponde aos xij do problema, e sao 0 ou 1
 x = {}
-for i in range(num_planetas):
-    for j in range(num_planetas):
-        x[i, j] = solver.IntVar(0, 1, '')
+for i in range(num_cidades):
+    for j in range(num_cidades):
+        x[i, j] = solver.IntVar(0.0, 1.0, '')
+
+
+
+mp_variables = []
+mp_values = []
+for i in range(0, round(len(seq_vis) * 0.92)):
+	if(seq_vis[i] != -1):
+		x[i, seq_vis[i]] = solver.IntVar(1.0, 1.0, '')
+		# mp_variables.append(x[i, seq_vis[i]])
+		# mp_values.append(1.0)
+		
+
+
+
+# mp_variables = [x[0, 1], x[1, 5], x[5, 9], x[9, 10], x[10, 11], x[11, 12], x[12, 13], x[13, 16], x[16, 17], x[17, 18], x[18, 21], x[21, 22], x[22, 28], x[28, 27]]
+# mp_values = [1.0 , 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+# solver.SetHint(mp_variables, mp_values)
+# x[0, 1] = solver.IntVar(1, 1, '')
+# x[1, 5] = solver.IntVar(1, 1, '')
+# x[5, 9] = solver.IntVar(1, 1, '')
+# x[9, 10] = solver.IntVar(1, 1, '')
+# x[10, 11] = solver.IntVar(1, 1, '')
+# x[11, 12] = solver.IntVar(1, 1, '')
+# x[12, 13] = solver.IntVar(1, 1, '')
+# x[13, 16] = solver.IntVar(1, 1, '')
+# x[16, 17] = solver.IntVar(1, 1, '')
+# x[17, 18] = solver.IntVar(1, 1, '')
+# x[18, 21] = solver.IntVar(1, 1, '')
+# x[21, 22] = solver.IntVar(1, 1, '')
+# x[22, 28] = solver.IntVar(1, 1, '')
+# x[28, 27] = solver.IntVar(1, 1, '')
 
 y = {}
-for i in range(num_planetas):
-   y[i] = solver.IntVar(1, (num_planetas - 1), '')
+for i in range(num_cidades):
+	y[i] = solver.IntVar(1, (num_cidades - 1), '')
 # --------------- End Definicoes das variaveis --------------
 
 
 
 # ------------------------  Restricoes ----------------------
 # Garante que cada vertice so pode ser visitado uma unica vez
-for i in range(num_planetas):
-    solver.Add(solver.Sum([x[i, j] for j in range(num_planetas)]) == 1)
+for i in range(num_cidades):
+    solver.Add(solver.Sum([x[i, j] for j in range(num_cidades)]) == 1)
 
 # Garante que, ao sair de uma galaxia, tenha-se somente uma outra galaxia como destino
-for j in range(num_planetas):
-    solver.Add(solver.Sum([x[i, j] for i in range(num_planetas)]) == 1)
+for j in range(num_cidades):
+    solver.Add(solver.Sum([x[i, j] for i in range(num_cidades)]) == 1)
 
 # Garante que nao havera subrotas
-for i in range(1, num_planetas):
-   for j in range(1,  num_planetas):
-      solver.Add((x[i, j] * (num_planetas - 1) + y[i] - y[j]) <= (num_planetas - 2))
+for i in range(1, num_cidades):
+   for j in range(1,  num_cidades):
+      solver.Add((x[i, j] * (num_cidades - 1) + y[i] - y[j]) <= (num_cidades - 2))
 # --------------------- End Restricoes ---------------------
 
 
 
 # --------------------  Funcao Objetivo --------------------
 objective_terms = []
-for i in range(num_planetas):
-    for j in range(num_planetas):
+for i in range(num_cidades):
+    for j in range(num_cidades):
         objective_terms.append(custos[i][j] * x[i, j])
 solver.Minimize(solver.Sum(objective_terms))
 # ------------------  End Funcao Objetivo ------------------
@@ -96,18 +144,18 @@ solver.Minimize(solver.Sum(objective_terms))
 status = solver.Solve()
 
 if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
-  print('Total cost = ', round(solver.Objective().Value(), 1), '\n')
-  count = 0
-  i = 0
-  while (count < 5):
-    for j in range(num_planetas):
-        # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
-        if x[i, j].solution_value() == 1.0:
-            print('%d -> %d.  Cost = %d' %
-               (i, j, custos[i][j]))
-            dot.edge(str(i), str(j), constraint='false', label=str(custos[i][j])) # Adiciona as arestas no grafo
-            i = j
-            break
-    count += 1
+	print('Total cost = ', round(solver.Objective().Value(), 1), '\n')
+	count = 0
+	i = 0
+	while (count < len(custos)):
+		for j in range(num_cidades):
+			# Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
+			if x[i, j].solution_value() == 1.0:
+				print('%d -> %d.  Cost = %d' %
+					(i, j, custos[i][j]))
+				#dot.edge(str(i), str(j), constraint='false', label=str(custos[i][j])) # Adiciona as arestas no grafo
+				i = j
+				break
+		count += 1
 
-dot.render('grafo') # Salva o arquivo no formato SVG
+#dot.render('grafo') # Salva o arquivo no formato SVG
